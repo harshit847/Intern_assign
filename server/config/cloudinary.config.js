b7da0b83b7d2dd548d014e5401e9be48f7d70956
harshit847 <harshitrai331@gmail.com>
2025-06-22 15:59:16 +0530

    New Fix

--- server/config/cloudinary.config.js
+++ server/config/cloudinary.config.js
@@ -1,8 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: 'drxlmu23f',
-  api_key: '562513814165386',
-  api_secret: 'szvO5csejZVen8vZWogWzV7STM8',
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 export default cloudinary;
